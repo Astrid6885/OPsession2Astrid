0afa8c12059cd0d94e821af5f73082c5b4a9cbe7
Astrid6885 <b00831189@essec.edu>
2026-09-16 15:38:10 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,3 +3,5 @@ echo "Starting installation..."
 echo "Updating package lists..."
 echo "Installing git..."
 echo "Installation complete!"
+
+PART 7 : No this file is not present on the main Branch because i created it on another branch, the feature script one. 
